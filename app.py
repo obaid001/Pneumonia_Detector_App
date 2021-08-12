129719a7cb6d61cf805d375922f2153ab673c6b0
obaid001 <88502279+obaid001@users.noreply.github.com>
2021-08-12 06:40:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 import streamlit as st
-from keras.preprocessing.image import load_img
-from keras.preprocessing.image import img_to_array
-from skimage.io import imshow, imsave, imread
 from skimage.transform import resize
 from skimage.color import gray2rgb
-from keras.models import load_model
 import numpy as np
 import tensorflow as tf
 from PIL import Image

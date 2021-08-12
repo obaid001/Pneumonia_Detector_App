18ca3f09b2652bdf3c5c533160032b7c6c9be6ac
obaid001 <88502279+obaid001@users.noreply.github.com>
2021-08-12 06:05:59 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,56 @@
+import streamlit as st
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from skimage.io import imshow, imsave, imread
+from skimage.transform import resize
+from skimage.color import gray2rgb
+from keras.models import load_model
+import numpy as np
+import tensorflow as tf
+from PIL import Image
+import model_arch
+#model = load_model('pneu.h5')
+model = model_arch.model_arch()
+weight1 = np.load('weight1.npy', allow_pickle=True)
+weight2 = np.load('weight2.npy', allow_pickle=True)
+z = weight1.tolist() + weight2.tolist()
+for i in tqdm(range(len(z))):
+  model.weights[i].assign(tf.reshape(z[i], model.weights[i].numpy().shape))
+  
+st.title('Pneumonia Detector')
+
+img_file = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+
+def process(image):
+    if image is not None:
+        img = np.array(Image.open(image))
+        img = img/255
+        img = resize(img, (256, 256))
+        return img
+    else:
+        st.text('Upload a Image')	
+if img_file is not None:
+    img = process(img_file)
+
+    st.image(img, clamp = True, channels = 'RGB', caption = 'Input Image')
+
+def predict(image):
+    img = gray2rgb(image)
+    img = np.expand_dims(img, axis = 0)   
+    pred = model.predict_classes(img)[0]
+    if pred == 1:
+        class_ = 'Pneumonia'
+    else:
+        class_ = 'Normal'
+    
+    st.title('Result: ' + class_)
+
+if img_file is not None:
+    predict(img)
+
+
+
+
+
+
+
---
+++ model_arch.py
@@ -0,0 +1,41 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import *
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.applications import DenseNet121
+def model_arch():
+    model = Sequential()
+    base = DenseNet121(include_top = False, input_shape = (256, 256, 3), pooling = 'avg', classes = 2)
+    for l in base.layers:
+        l.trainable = False
+
+    model.add(base)
+
+    model.add(Flatten())
+    model.add(BatchNormalization())
+
+    model.add((Dense(256, activation = 'relu')))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.5))
+
+    model.add((Dense(128, activation = 'relu')))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.5))
+
+    model.add((Dense(64, activation = 'relu')))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.5))
+
+    model.add((Dense(32, activation = 'relu')))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.5))
+
+    model.add((Dense(16, activation = 'relu')))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.5))
+
+    model.add((Dense(1, activation = 'sigmoid')))
+
+    opt = Adam()
+
+    model.compile(optimizer = opt, loss = 'binary_crossentropy', metrics = ['accuracy'])
+    return  model
